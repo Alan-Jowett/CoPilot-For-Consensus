5af7a6cbe66b3c4559f686b446b283047d6d1dbb
Alan Jowett <alan.jowett@microsoft.com>
2025-12-18 19:08:11 -0800

    Fix adapter installer

--- adapters/scripts/install_adapters.py
+++ adapters/scripts/install_adapters.py
@@ -41,7 +41,7 @@ ADAPTER_DEPENDENCIES = {
     "copilot_storage": ["copilot_schema_validation"],
     "copilot_schema_validation": ["copilot_storage"],
     "copilot_events": ["copilot_schema_validation", "copilot_storage"],
-    "copilot_config": ["copilot_schema_validation", "copilot_storage"],
+    "copilot_config": ["copilot_schema_validation", "copilot_storage", "copilot_secrets"],
     "copilot_secrets": [],
     "copilot_chunking": ["copilot_schema_validation", "copilot_storage"],
     "copilot_logging": [],
