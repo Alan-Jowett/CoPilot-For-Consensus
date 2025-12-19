e97df77a899ef0b99898ede6a3629145c3c21e8f
Alan Jowett <alan.jowett@microsoft.com>
2025-12-19 11:30:45 -0800

    fix: remove merge conflict marker from AppLayout.tsx

--- ui/src/ui/AppLayout.tsx
+++ ui/src/ui/AppLayout.tsx
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2025 Copilot-for-Consensus contributors
 
-<<<<<<< HEAD
 import { Outlet, Link, useLocation } from 'react-router-dom'
 import { ThemeToggle } from '../components/ThemeToggle'
 
