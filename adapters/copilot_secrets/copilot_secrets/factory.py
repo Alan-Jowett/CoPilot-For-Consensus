6344f669dc417460116659e14878234e0582fc50
Alan Jowett <alan.jowett@microsoft.com>
2026-01-15 14:17:38 -0800

    copilot_secrets: typed factory + pyright gate
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- adapters/copilot_secrets/copilot_secrets/factory.py
+++ adapters/copilot_secrets/copilot_secrets/factory.py
@@ -3,12 +3,32 @@
 
 """Factory for creating secret providers."""
 
+from typing import TypeAlias
+
 from copilot_config.adapter_factory import create_adapter
-from copilot_config.generated.adapters.secret_provider import AdapterConfig_SecretProvider
+from copilot_config.generated.adapters.secret_provider import (
+    AdapterConfig_SecretProvider,
+    DriverConfig_SecretProvider_AzureKeyVault,
+    DriverConfig_SecretProvider_Local,
+)
 
 from .azurekeyvault_provider import AzureKeyVaultProvider
 from .local_provider import LocalFileSecretProvider
 from .provider import SecretProvider
+
+_DriverConfig: TypeAlias = DriverConfig_SecretProvider_Local | DriverConfig_SecretProvider_AzureKeyVault
+
+
+def _build_local(config: _DriverConfig) -> SecretProvider:
+    if not isinstance(config, DriverConfig_SecretProvider_Local):
+        raise TypeError("driver config must be DriverConfig_SecretProvider_Local")
+    return LocalFileSecretProvider.from_config(config)
+
+
+def _build_azure_key_vault(config: _DriverConfig) -> SecretProvider:
+    if not isinstance(config, DriverConfig_SecretProvider_AzureKeyVault):
+        raise TypeError("driver config must be DriverConfig_SecretProvider_AzureKeyVault")
+    return AzureKeyVaultProvider.from_config(config)
 
 
 def create_secret_provider(
@@ -32,7 +52,7 @@ def create_secret_provider(
         get_driver_type=lambda c: c.secret_provider_type,
         get_driver_config=lambda c: c.driver,
         drivers={
-            "local": LocalFileSecretProvider.from_config,
-            "azure_key_vault": AzureKeyVaultProvider.from_config,
+            "local": _build_local,
+            "azure_key_vault": _build_azure_key_vault,
         },
     )
